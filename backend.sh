5f9e7982f64cf8e1abb9220047b381616ef29056
akr9757 <anilkumareddy9757@gmail.com>
2024-07-26 11:38:02 +0530

    backend shell script

--- backend.sh
+++ backend.sh
@@ -7,7 +7,6 @@ mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
 unzip /tmp/backend.zip
-cd /app
 npm install
 dnf install mysql -y
 mysql -h 172.31.45.171 -uroot -pExpenseApp@1 < /app/schema/backend.sql
