f941fd4ad9ac20eb823f7feaccd97964922f72d8
akr9757 <anilkumareddy9757@gmail.com>
2024-07-26 15:11:50 +0530

    backend shell script

--- backend.sh
+++ backend.sh
@@ -3,6 +3,7 @@ dnf module enable nodejs:20 -y
 dnf install nodejs -y
 cp backend.service /etc/systemd/system/backend.service
 useradd expense
+rm -rf /app
 mkdir /app
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
