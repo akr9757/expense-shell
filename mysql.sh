0b5492fdfc90af04838542cc4f2355f474442726
akr9757 <anilkumareddy9757@gmail.com>
2024-07-27 10:41:02 +0530

    backend shell script

--- mysql.sh
+++ mysql.sh
@@ -2,5 +2,6 @@ dnf module disable mysql -y
 cp mysql.repo /etc/yum.repos.d/mysql.repo
 dnf install mysql-community-server -y
 mysql_secure_installation --set-root-pass ExpenseApp@1
+mysql -uroot -pExpenseApp@1
 systemctl enable mysqld
 systemctl restart mysqld
