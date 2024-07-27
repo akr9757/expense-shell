1e819874646564b54a5c2127dd589bf824c28575
akr9757 <anilkumareddy9757@gmail.com>
2024-07-27 10:37:18 +0530

    backend shell script

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,8 @@
 dnf install nginx -y
+cp expense.conf /etc/nginx/default.d/expense.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-vim /etc/nginx/default.d/expense.conf
 systemctl enable nginx
 systemctl restart nginx
